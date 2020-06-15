cf2a697aa6528f738e03a27d53b24235d5bc0157
ELSA <elsa.steinfath@stud.uni-goettingen.de>
2020-06-15 14:59:13 +0000

    changed function name to get_colour_by_dice_roll

--- coloursDice.py
+++ coloursDice.py
@@ -17,13 +17,13 @@ def write_log_file(outputfilename, data):
     f.write("My randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
     f.close()
 
-def get_colour_by_dice(spots):
+def get_colour_by_dice_roll(spots):
     colors=['green','red','yellow','pink','black','blue']
     return colors[spots]
 
 if __name__ == "__main__":
     outputfilename = "randomNumber_color"
     roll = get_random_number(1, 6)
-    color = get_colour_by_dice(roll)
+    color = get_colour_by_dice_roll(roll)
     write_log_file(outputfilename, [roll, color])
     
