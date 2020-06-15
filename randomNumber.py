208207fe584db0283e538a9e9a5d377b65d66c92
ELSA <elsa.steinfath@stud.uni-goettingen.de>
2020-06-15 14:15:07 +0000

    replaced my with your

--- randomNumber.py
+++ randomNumber.py
@@ -15,7 +15,7 @@ def get_random_number(start, end):
 
 def write_log_file(outputfilename, data):
     f = open(outputfilename + ".log", "a")
-    f.write("My randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
+    f.write("Your randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
     f.close()
 
 
