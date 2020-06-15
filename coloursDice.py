be1117878f2356148318a9c0b0e869db62e9a282
ELSA <elsa.steinfath@stud.uni-goettingen.de>
2020-06-15 13:32:32 +0000

    created coloursDice.py which returns number from 1 til 6 and a corrsponding color ,and changed name of randomNumber2.py

---
+++ coloursDice.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# team: Daniel, Elsa
+
+import random
+import time
+import sys
+
+def get_random_number(start, end):
+    num = random.randint(start, end)
+    return num
+
+
+def write_log_file(outputfilename, data):
+    f = open(outputfilename + ".log", "a")
+    f.write("My randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
+    f.close()
+
+def get_colour_by_dice(spots):
+    colors=['green','red','yellow','pink','black','blue']
+    return colors[spots]
+
+if __name__ == "__main__":
+    outputfilename = "randomNumber_color"
+    roll = get_random_number(1, 6)
+    color = get_colour_by_dice(roll)
+    write_log_file(outputfilename, [roll, color])
+    
